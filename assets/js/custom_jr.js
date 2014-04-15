3e9c7a3b3ed1d14b434b254f293d3b4c5836e819
Jordan <JRiddick@Gmail.com>
2014-04-15 16:56:48 -0400

    Removing console.log statements

--- assets/js/custom_jr.js
+++ assets/js/custom_jr.js
@@ -83,7 +83,6 @@
     //~~~~~~~~~~~~~~~~~~~~~PRELOAD GIRD IMAGES AND DISPLAY ROW BY ROW~~~~~~~~~~~~~\\
 
     function showRow(_projectIndex){
-      console.log("showRow " + _projectIndex);
       $('#project_'+ (_projectIndex-2) + '').toggle();
       $('#project_'+ (_projectIndex-1) + '').toggle();
       $('#project_'+ _projectIndex + '').toggle();
@@ -183,7 +182,6 @@
    	var regexEmail = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
  	  var email = "";
      $('body').on('click', '#submitForm', function(){
-      console.log("called!")
      	var form = new FormData($('#myform')[0]);
    	 	email = $('#contact_email').val();
   		if (regexEmail.test(email)){
@@ -192,7 +190,6 @@
   				return;
   			}
   			else{  		
-          console.log('ajax called');	
   				$.ajax({
 				    type: "POST",
 				    url: "./assets/php/email_v2.php",
@@ -204,12 +201,10 @@
                 }
                 else{
                   alert("Oops, looks like out email is down!");
-                  console.log(res);
                 }
 				    },
             error: function(res){
               alert("error");
-              console.log(res);
             },
 				    cache: false,
 	                contentType: false,
